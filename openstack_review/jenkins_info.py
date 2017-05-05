d182d8074976279f1e322419e0086e479b29b1c8
Arx Cruz <arxcruz@redhat.com>
2017-05-05 11:29:57 +0200

    Adding jenkins_info.py
    This is the basic model to be used on each row. It's inherit from
    GObject so we can take advantage of signals, getters and setters

---
+++ openstack_review/jenkins_info.py
@@ -0,0 +1,14 @@
+from gi.repository import GObject
+
+
+class JenkinsInfo(GObject.GObject):
+    url = GObject.Property(type=str, flags=GObject.PARAM_READWRITE)
+    change_id = GObject.Property(type=str, flags=GObject.PARAM_READWRITE)
+    verified = GObject.Property(type=bool, flags=GObject.PARAM_READWRITE,
+                                default=True)
+    description = GObject.Property(type=str, flags=GObject.PARAM_READWRITE)
+    subject = GObject.Property(type=str, flags=GObject.PARAM_READWRITE)
+
+    def __init__(self):
+        GObject.GObject.__init__(self)
+

5af90d73e2324b03967a52404cd2278741b0d123
Arx Cruz <arxcruz@redhat.com>
2017-05-10 11:28:46 +0200

    Few changes:
    * Removing gobject_worker.py since it's no longer needed
    * Adding logging support
    * Removing the call to GObjectWorker and calling directly threading

--- openstack_review/application.py
+++ openstack_review/application.py
@@ -1,8 +1,10 @@
 import gi
+import logging
+import threading
 
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 from jenkins_list_view import JenkinsListBox
 from jenkins_info_list import JenkinsInfoList
 from gobject_worker import GObjectWorker
@@ -27,11 +29,16 @@ class ListBoxWindow(Gtk.Window):
         box_outer.pack_start(scroll, True, True, 0)
         scroll.add(listbox)
 
-        self.worker = GObjectWorker()
+        # self.worker = GObjectWorker()
         self.mqtt = OpenstackMqtt(info_list)
-        self.worker.send(self.mqtt.start)
+        thread = threading.Thread(target=self.mqtt.start)
+        thread.daemon = True
+        thread.start()
+
+        # self.worker.send(self.mqtt.start)
         self.set_default_size(300, 600)
 
+logging.basicConfig(level=logging.DEBUG)
 
 win = ListBoxWindow()
 win.connect("delete-event", Gtk.main_quit)

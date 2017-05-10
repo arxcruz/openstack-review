aab01463496601e8dfa2f8fc029a358b30ef582a
Arx Cruz <arxcruz@redhat.com>
2017-05-10 11:57:41 +0200

    Switch from Gtk.Label to Gtk.TextView
    Changed description to be a Gtk.TextView it's better to visualize
    because this way you don't have big rows for big comments.

--- openstack_review/jenkins_row.py
+++ openstack_review/jenkins_row.py
@@ -3,20 +3,21 @@ from gi.repository import GObject
 
 from jenkins_info import JenkinsInfo
 
+import logging
+
+LOG = logging.getLogger(__name__)
+
 
 class JenkinsRow(Gtk.ListBoxRow):
     def __init__(self, j_info):
-        self.j_info = None
         Gtk.ListBoxRow.__init__(self)
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.image = Gtk.Image.new_from_stock('gtk-no', Gtk.IconSize.BUTTON)
         self.subject = Gtk.Label()
         self.subject.set_line_wrap(True)
         self.subject.set_xalign(0)
-        self.subject.set_use_markup(True)
-        self.description = Gtk.Label()
-        self.description.set_line_wrap(True)
-        self.description.set_xalign(0)
+        self.description = Gtk.TextView()
+        self.textbuffer = self.description.get_buffer()
         self.url = Gtk.LinkButton()
         self.url.set_label('Review this change')
 
@@ -26,31 +27,35 @@ class JenkinsRow(Gtk.ListBoxRow):
         box.pack_start(hbox, True, True, 0)
         expander = Gtk.Expander.new('Details')
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        hbox.pack_start(self.description, True, True, 6)
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_size_request(-1, 100)
+        scrolled_window.add(self.description)
+        hbox.pack_start(scrolled_window, True, True, 6)
         expander.add(hbox)
         box.pack_start(expander, True, True, 6)
         box.pack_start(self.url, True, True, 6)
-        self.jenkis_info = j_info
         self.add(box)
+        self.jenkins_info = j_info
+        LOG.debug('Row created')
 
     @GObject.Property(type=JenkinsInfo)
     def jenkins_info(self):
-        return self.j_info
+        return self._j_info
 
     @jenkins_info.setter
     def jenkis_info(self, value):
-        self.j_info = value
-        self.j_info.subject = '<b>{}</b>'.format(self.j_info.subject)
-        self.j_info.bind_property('subject', self.subject, 'label',
-                                  GObject.BindingFlags.DEFAULT |
-                                  GObject.BindingFlags.SYNC_CREATE)
-        self.j_info.bind_property('description', self.description, 'label',
-                                  GObject.BindingFlags.DEFAULT |
-                                  GObject.BindingFlags.SYNC_CREATE)
-        self.j_info.bind_property('url', self.url, 'uri',
-                                  GObject.BindingFlags.DEFAULT |
-                                  GObject.BindingFlags.SYNC_CREATE)
-        self.j_info.connect('notify::verified', self.on_notify_verify)
+        LOG.debug('Setting jenkins_info: {}'.format(value))
+        self._j_info = value
+        self._j_info.bind_property('subject', self.subject, 'label',
+                                   GObject.BindingFlags.DEFAULT |
+                                   GObject.BindingFlags.SYNC_CREATE)
+        self._j_info.bind_property('description', self.textbuffer, 'text',
+                                   GObject.BindingFlags.DEFAULT |
+                                   GObject.BindingFlags.SYNC_CREATE)
+        self._j_info.bind_property('url', self.url, 'uri',
+                                   GObject.BindingFlags.DEFAULT |
+                                   GObject.BindingFlags.SYNC_CREATE)
+        self._j_info.connect('notify::verified', self.on_notify_verify)
 
     def on_notify_verify(self, obj, gparamstring):
         print 'Change status: {}'.format(obj.verified)
@@ -58,3 +63,4 @@ class JenkinsRow(Gtk.ListBoxRow):
             self.image.set_from_stock('gtk-yes', Gtk.IconSize.BUTTON)
         else:
             self.image.set_from_stock('gtk-no', Gtk.IconSize.BUTTON)
+        self.changed()

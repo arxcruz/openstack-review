eea93c5d766e3e92bb133b11ba18fa35b203f800
Arx Cruz <arxcruz@redhat.com>
2017-05-10 10:19:43 +0200

    Adding sort_func and debug messages

--- openstack_review/jenkins_list_view.py
+++ openstack_review/jenkins_list_view.py
@@ -1,6 +1,10 @@
+import logging
+
 from gi.repository import Gtk
 
 from jenkins_row import JenkinsRow
+
+LOG = logging.getLogger(__name__)
 
 
 class JenkinsListBox(Gtk.ListBox):
@@ -11,18 +15,25 @@ class JenkinsListBox(Gtk.ListBox):
                                self.on_jenkins_info_added_cb)
         self.info_list.connect('jenkins-info-deleted',
                                self.on_jenkins_info_deleted_cb)
+        self.set_sort_func(self.sort_func, None, False)
 
     def on_jenkins_info_added_cb(self, obj, jenkins_info):
-        # Add row
-        print 'Entrou'
+        LOG.debug('Row added with info: {}'.format(jenkins_info))
         row = JenkinsRow(jenkins_info)
         self.insert(row, 0)
         row.show_all()
 
     def on_jenkins_info_deleted_cb(self, obj, jenkins_info):
+        LOG.debug('Row deleted with info: {}'.format(jenkins_info))
         childrens = self.get_children()
         for row in childrens:
             print 'Checking to delete change: {}'.format(
                     row.jenkins_info.change_id)
             if row.jenkins_info == jenkins_info:
                 row.destroy()
+
+    def sort_func(self, row1, row2, data, notify_destroy):
+        LOG.debug('Entering sort_func')
+        LOG.debug('Row 1: {}'.format(row1.jenkins_info))
+        LOG.debug('Row 2: {}'.format(row2.jenkis_info))
+        return row1.jenkins_info.verified > row2.jenkins_info.verified
